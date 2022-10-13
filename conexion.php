5b6c1ee7359c19ffd4685dd7755e598f0669d2ca
jkarrenoo <jkarreno@gmail.com>
2022-10-12 20:57:46 -0500

    Se agrego archivo para respaldo de base de datos

--- conexion.php
+++ conexion.php
@@ -4,9 +4,8 @@
 //"Esperanza2021#") or die('Cannot connect to the database because: ' . mysqli_error());
 //mysqli_select_db ($conn, "kraps_laesperanza");
 
-$conn=mysqli_connect ("localhost", "kraps_devlaesperanza",
-"Desarrollo21#") or die('Cannot connect to the database because: ' . mysqli_error());
-mysqli_select_db ($conn, "kraps_dev_laesperanza");
+$conn=mysqli_connect ("sql5c75d.carrierzone.com", "gpotciapps511228", "Gpotci15#") or die('Cannot connect to the database because: ' . mysqli_error());
+mysqli_select_db ($conn, "gpotcifiles_gpotciapps511228");
 
-//@jkarreno 2020
+//@jkarreno 2022
 ?>
