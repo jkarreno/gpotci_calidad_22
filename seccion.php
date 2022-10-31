8f9aacd8baf16a3d04c848b4690fbe040c548460
jkarrenoo <jkarreno@gmail.com>
2022-10-30 19:28:27 -0600

    Se creo la vista de los archivos de la sección

--- seccion.php
+++ seccion.php
@@ -31,17 +31,24 @@ $cadena.='<div class="c100" style="display: flex; flex-wrap:wrap;">
                 </tr>
             </thead>
             <tbody>';
-$ResFiles($conn, "SELECT * FROM Archivos WHERE Seccion='".$_POST["seccion"]."' ORDER BY Codigo ASC");
+$ResFiles=mysqli_query($conn, "SELECT * FROM Archivos WHERE Seccion='".$_POST["seccion"]."' ORDER BY Codigo ASC");
 $bgcolor="#ffffff"; $J=1;
 while($RResFiles=mysqli_fetch_array($ResFiles))
 {
-    if()$ext_r=explode('.', $nombre_archivo_r);
+    if($RResFiles["Archivo_r"]!='' AND $RResFiles["Extension_r"]!='')
+    {
+        if($RResFiles["Extension_r"]=='doc' OR $RResFiles["Extension_r"]=='docx'){$ico='<i class="fa-solid fa-file-word"></i>';}
+        elseif($RResFiles["Extension_r"]=='xls' OR $RResFiles["Extension_r"]=='xlsx'){$ico='<i class="fa-solid fa-file-excel"></i>';}
+        elseif($RResFiles["Extension_r"]=='pdf'){$ico='<i class="fa-solid fa-file-pdf"></i>';}
+
+        $archivo='<a href="files/'.$_POST["seccion"].'/'.$RResFiles["Archivo_r"].'" target="_blank">'.$ico.'</a>';
+    }
     $cadena.='      <tr style="background: '.$bgcolor.'" id="row_'.$J.'">
                         <td onmouseover="row_'.$J.'.style.background=\'#badad8\'" onmouseout="row_'.$J.'.style.background=\''.$bgcolor.'\'" align="center" class="texto" valign="middle">'.$J.'</td>
                         <td onmouseover="row_'.$J.'.style.background=\'#badad8\'" onmouseout="row_'.$J.'.style.background=\''.$bgcolor.'\'" align="center" class="texto" valign="middle">'.$RResFiles["Codigo"].'</td>
                         <td onmouseover="row_'.$J.'.style.background=\'#badad8\'" onmouseout="row_'.$J.'.style.background=\''.$bgcolor.'\'" align="center" class="texto" valign="middle">'.$RResFiles["Nombre"].'</td>
-                        <td onmouseover="row_'.$J.'.style.background=\'#badad8\'" onmouseout="row_'.$J.'.style.background=\''.$bgcolor.'\'" align="center" class="texto" valign="middle"><a href="'.$RResFiles["Url_d"].'" target="_blank"><i class="fa-solid fa-file-pdf"></i></a></td>
-                        <td onmouseover="row_'.$J.'.style.background=\'#badad8\'" onmouseout="row_'.$J.'.style.background=\''.$bgcolor.'\'" align="center" class="texto" valign="middle"><a href="files/'.$_POST["seccion"].'/'.$RResFiles["Archivo_r"].'" target="_blank">'.$ico.'</a><td>
+                        <td onmouseover="row_'.$J.'.style.background=\'#badad8\'" onmouseout="row_'.$J.'.style.background=\''.$bgcolor.'\'" align="center" class="texto" valign="middle"><a href="'.$RResFiles["Url_d"].'" target="_blank"><i class="fa-brands fa-google-drive"></i></a></td>
+                        <td onmouseover="row_'.$J.'.style.background=\'#badad8\'" onmouseout="row_'.$J.'.style.background=\''.$bgcolor.'\'" align="center" class="texto" valign="middle">'.$archivo.'</td>
                     </tr>';
 
     if($bgcolor=="#ffffff"){$bgcolor="#cccccc";}
